871bf17da121dd0910b7808c9b196ee6b65582e8
Maurizio De Santis <desantis.maurizio@gmail.com>
2013-10-20 01:49:12 +0200

    Support for rbx-2.1

--- etc/rbenv.d/which/rbx.bash
+++ etc/rbenv.d/which/rbx.bash
@@ -1,3 +1,9 @@
-if [ ! -x "$RBENV_COMMAND_PATH" ] && [[ $RBENV_VERSION =~ rbx-2 ]]; then
-  export RBENV_COMMAND_PATH="${RBENV_ROOT}/versions/${RBENV_VERSION}/1.9/bin/${RBENV_COMMAND}"
+if [ ! -x "$RBENV_COMMAND_PATH" ]; then
+  if [[ $RBENV_VERSION =~ rbx-2.1 ]]; then
+    export RBENV_COMMAND_PATH="${RBENV_ROOT}/versions/${RBENV_VERSION}/gems/bin/${RBENV_COMMAND}"
+  else
+    if [[ $RBENV_VERSION =~ rbx-2 ]]; then
+      export RBENV_COMMAND_PATH="${RBENV_ROOT}/versions/${RBENV_VERSION}/1.9/bin/${RBENV_COMMAND}"
+    fi
+  fi
 fi
